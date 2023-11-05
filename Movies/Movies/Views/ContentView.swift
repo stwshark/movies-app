3be9ecd26c34e80c8d037c682f26fb0328be53b5
Stanislaw <stanislaw.ios.dev@gmail.com>
2023-11-05 17:03:24 +0300

    ContentView  layout

--- Movies/Movies/Views/ContentView.swift
+++ Movies/Movies/Views/ContentView.swift
@@ -4,23 +4,66 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text("THE MOVIES")
-                    .font(.system(size: 40, weight: .bold, design: .serif))
-                    .foregroundStyle(.red)
-                NavigationLink(destination: UpcomingView()) {
-                    VStack{
-                        Text("Upcoming movies")
+                Title(title: "THE MOVIES")
+                
+                VStack(spacing: 20) {
+                    NavigationLink(destination: UpcomingView()) {
+                        CustomNavigationTitle(text: "Upcoming movies")
+                    }
+                    NavigationLink(destination: UpcomingView()) {
+                        CustomNavigationTitle(text: "Best rated")
+                    }
+                    NavigationLink(destination: UpcomingView()) {
+                        CustomNavigationTitle(text: "Search")
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(.ultraThinMaterial)
+                }
+                
                 Spacer()
             }
-            .foregroundColor(.white)
+            .padding(.horizontal)
             .background{
                 Image("joker3")
                     .resizable()
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
             }
+        }
+    }
+}
+
+struct Title: View {
+    let title: String
+    var body: some View {
+        Text(title)
+            .font(.system(size: 40, weight: .bold, design: .serif))
+            .foregroundStyle(.red)
+    }
+}
+
+struct CustomNavigationTitle: View {
+    let text: String
+    var body: some View {
+        HStack {
+            Text(text)
+                .font(.system(size: 20, weight: .bold, design: .serif))
+                .foregroundStyle(.white)
+            Spacer()
+            Text("->")
+                .font(.system(size: 20, weight: .bold, design: .serif))
+                .foregroundStyle(.white)
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .frame(height: 60)
+        .background {
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color("deepblue"))
         }
     }
 }
